e05cc38e1f637be328d12227f36de27918ffe53d
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-08-11 21:16:43 +0530

    Create Square Pattern Alternate 0 1.c

---
+++ Patterns/Square Pattern Alternate 0 1.c
@@ -0,0 +1,47 @@
+/*
+Input
+
+Input rows: 5
+Input columns: 5
+Output
+
+11111
+00000
+11111
+00000
+11111
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols, i, j;
+
+    /* Input rows and columns from user */
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=cols; j++)
+        {
+            // Print 1 if current row is odd
+            if(i%2 == 1)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
