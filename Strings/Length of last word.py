c7784d7f7ab6c40d68bdf95ca7afe5f52ca0f5c8
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-08-21 13:06:26 +0530

    Create Length of last word.py

---
+++ Strings/Length of last word.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        
+        if len(words) == 0:
+            return 0
+
+        return len(words[-1])
+        
+            
