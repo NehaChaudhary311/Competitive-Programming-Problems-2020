257e7a2c0405c3d477446581b7366bcfa68d128d
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-08-23 21:53:34 +0530

    Create Convert binary number in LL to Integer.cpp

---
+++ Bit Manipulation/Convert binary number in LL to Integer.cpp
@@ -0,0 +1,55 @@
+/*
+Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+Input: head = [0]
+Output: 0
+
+Example 3:
+Input: head = [1]
+Output: 1
+
+Example 4:
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+
+Example 5:
+Input: head = [0,0]
+Output: 0
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        if(head==NULL)
+            return 0;
+        int ans=0;
+
+        ListNode* temp=head;
+
+
+        while(temp){
+
+            ans=ans<<1;    //left shift
+            ans+=temp->val;  
+            temp=temp->next;
+        }
+        return ans;
+           
+       
+    }
+};
