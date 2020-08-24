edf4d9e9fd2a422bf4d15ee6d2620cf44cd3d777
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-08-24 16:59:26 +0530

    Create Prime Number of Set Bits in Binary Representation.cpp

---
+++ Bit Manipulation/Prime Number of Set Bits in Binary Representation.cpp
@@ -0,0 +1,55 @@
+/*
+
+Given two integers L and R, find the count of numbers in the range [L, R] (inclusive) having a prime number of set bits in their binary representation.
+(Recall that the number of set bits an integer has is the number of 1s present when written in binary. For example, 21 written in binary is 10101 which has 3 set bits. Also, 1 is not a prime.)
+
+Example 1:
+Input: L = 6, R = 10
+Output: 4
+Explanation:
+6 -> 110 (2 set bits, 2 is prime)
+7 -> 111 (3 set bits, 3 is prime)
+9 -> 1001 (2 set bits , 2 is prime)
+10->1010 (2 set bits , 2 is prime)
+
+Example 2:
+Input: L = 10, R = 15
+Output: 5
+Explanation:
+10 -> 1010 (2 set bits, 2 is prime)
+11 -> 1011 (3 set bits, 3 is prime)
+12 -> 1100 (2 set bits, 2 is prime)
+13 -> 1101 (3 set bits, 3 is prime)
+14 -> 1110 (3 set bits, 3 is prime)
+15 -> 1111 (4 set bits, 4 is not prime)
+
+*/
+
+class Solution {
+public:
+     bool isPrime(int number) {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i <= sqrt(number); i+=2){
+            if (number % i == 0)
+                return false;
+
+        }
+        
+        return true;        
+    }
+    int countPrimeSetBits(int L, int R) {
+        int count_out = 0;
+        
+        for (int i = 0;L <= R; L++) {
+            int count = __builtin_popcount(L);
+            if (isPrime(count)) {
+                count_out++;
+            }
+        }
+        
+        return count_out;
+    }
+};
