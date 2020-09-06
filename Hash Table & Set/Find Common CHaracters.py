1bef9017b6a56b98853af3841682bf1e804f0f64
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-09-06 12:38:56 +0530

    Update Find Common CHaracters.py

--- Hash Table & Set/Find Common CHaracters.py
+++ Hash Table & Set/Find Common CHaracters.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates). 
 For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
 
@@ -9,20 +9,24 @@ Output: ["e","l","l"]
 Example 2:
 Input: ["cool","lock","cook"]
 Output: ["c","o"]
-*/
+'''
 
 class Solution:
     def commonChars(self, A: List[str]) -> List[str]:
-        res = []
-        i = ord('a')
-        for _ in range(i+26):
-            c = chr(i)
-            for j in range(len(A)):
-                if c not in A[j]:
-                    i+=1
+        # result, list of all letters in common in all strings.
+        result = []
+        # go through all letters of a given word, check if any letters exist in all other words.
+        for letter in A[0]:
+            # flag to check if letter appears in all words.
+            flag = True
+            # check all words for current letter.
+            for i in range(len(A)):
+                if letter not in A[i]:
+                    flag = False
                     break
-            else:
-                res.append(c)
-                for k in range(len(A)):
-                    A[k]=A[k].replace(c,"",1)
-        return res
+                # remove each letter after checking to avoid false duplicates.
+                A[i] = A[i].replace(letter, "", 1)
+            if flag == True:
+                result.append(letter)
+        # return result.
+        return result
