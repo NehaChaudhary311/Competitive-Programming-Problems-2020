e3ada193c7ed7b701202a0702f8ddc60c4480a33
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-09-11 15:35:18 +0530

    Update Minimum add to make parenthesis valid.cpp

--- Stack/Minimum add to make parenthesis valid.cpp
+++ Stack/Minimum add to make parenthesis valid.cpp
@@ -1,3 +1,22 @@
+/*
+Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+
+Example 1:
+Input: "())"
+Output: 1
+
+Example 2:
+Input: "((("
+Output: 3
+
+Example 3:
+Input: "()"
+Output: 0
+
+Example 4:
+Input: "()))(("
+Output: 4
+*/
 class Solution {
 public:
     int minAddToMakeValid(string S) {
