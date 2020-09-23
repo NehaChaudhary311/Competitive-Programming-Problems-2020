284c70da7ce5bfcc5c566e30b00c8f51b8f2eb8e
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-09-23 17:30:48 +0530

    Create Number of Occurences.cpp

---
+++ Arrays/Number of Occurences.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int count = 0;
+	    if(n == 0) return 0;
+	    for(int i = 0; i<n ; i++){
+	        if(arr[i] > x)
+	            break;
+	       else{
+	           if(arr[i] == x)
+	                count++;
+	       }
+	    }
+	    return count;
+	}
+};
