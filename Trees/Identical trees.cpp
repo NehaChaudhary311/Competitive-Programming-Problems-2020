745a56001cd117cf62f5ad62c80cdda95e1c0cbc
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-09-30 13:53:34 +0530

    Simple recursion

---
+++ Trees/Identical trees.cpp
@@ -0,0 +1,35 @@
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+/* Should return true if trees with roots as r1 and 
+   r2 are identical */
+bool isIdentical(Node *r1, Node *r2)
+{
+    //Your Code here
+    if(r1==NULL&&r2==NULL)
+    {return true;}
+    else if(r1==NULL||r2==NULL)
+    {return false;}
+    else
+    {
+        if(r1->data!=r2->data)
+        {return false;}
+        else
+        {
+            return isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right);
+        }
+    }
+}
