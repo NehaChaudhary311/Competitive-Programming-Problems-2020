3b00ba4acc6447f150c8a0dc9492c6592c9982c6
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-09-30 13:49:52 +0530

    Create Binary Tree to DLL.cpp

---
+++ Trees/Binary Tree to DLL.cpp
@@ -0,0 +1,42 @@
+/* Structure for tree and linked list
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return head to the DLL
+
+Node *prevDLL = nullptr, *head = nullptr;
+void DLL(Node *root){
+    if(root == nullptr){
+        return;
+    }
+    
+    DLL(root->left);
+    if(head == nullptr){
+        head = root;
+    }
+    else{
+        prevDLL->right = root;
+        root->left = prevDLL;
+    }
+    prevDLL = root;
+    DLL(root->right);
+}
+Node * bToDLL(Node *root)
+{
+    // your code here
+    prevDLL = nullptr;
+    head = nullptr;
+    DLL(root);
+    return head;
+}
