faac0a28607d3560fd027b1da7851d59094ece32
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-10-12 15:45:47 +0530

    Create Rotate a linked list.cpp

---
+++ Linked List/Rotate a linked list.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *rotate(struct Node *head, int k);
+
+Node *insert() {
+    int n, i, value;
+    struct Node *temp, *head = NULL;
+    cin >> n;
+
+    for (i = 0; i < n; i++) {
+        cin >> value;
+        if (i == 0) {
+            head = new Node(value);
+            temp = head;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+    return head;
+}
+
+void printList(Node *node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void) {
+
+    int t, k;
+
+    scanf("%d", &t);
+    while (t--) {
+        Node *head = insert(); // insert function to build linked list
+        scanf("%d", &k);
+        head = rotate(head, k); // rotate linked list anti-clockwise  by k nodes
+        printList(head);        // print linked list after rotation
+    }
+    return (0);
+}
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+/*  This function should rotate list counter-clockwise
+    by k and return new head (if changed) */
+Node* rotate(Node* head, int k) {
+    // Your code here
+    if(k == 0)
+        return head;
+    Node* temp = head;
+    int n = 1;
+    while(n < k && temp != NULL) {
+        temp = temp->next;
+        n++;
+    }
+    if(temp == NULL)
+        return head;;
+    Node* kth = temp;
+    
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    
+    temp->next = head;
+    head = kth->next;
+    kth->next = NULL;
+    return head;  
+}
