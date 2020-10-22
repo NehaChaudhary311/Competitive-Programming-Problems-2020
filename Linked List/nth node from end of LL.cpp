30cf5cddca8eb920fa3ee96fb916885cccfd3457
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-10-22 13:20:10 +0530

    Two approaches

--- Linked List/nth node from end of LL.cpp
+++ Linked List/nth node from end of LL.cpp
@@ -1,3 +1,4 @@
+/* Total Length approach*/
 int getNthFromLast(Node *head, int n)
 {
        // Your code here
@@ -12,6 +13,33 @@ int getNthFromLast(Node *head, int n)
            head = head->next;
        }
        return head->data;
+}
+
+/*Two pointers approach*/
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       //Using two pointers - mainp & refp
+       if(head == NULL) return -1;
+        Node* mainp = head;
+        Node* refp = head;
+        int count = 0;
+        
+        //Move the refp to n nodes from head
+        while(count != n){
+            if(refp == NULL) return -1;
+            refp = refp->next;
+            count++;
+        }
+        
+        //if refp becomes null or reaches the end of LL, that means nth last node is first node
+        if(refp == NULL) return mainp->data;
+        //else move mainp and refp at same pace till refp becomes nULL
+        while(refp != NULL){
+            mainp = mainp->next;
+            refp = refp->next;
+        }
+        return mainp->data;
 }
 
 
