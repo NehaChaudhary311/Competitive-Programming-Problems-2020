fc632c071b4edb8c8a9f213cb7b292d33913fe6d
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-12-05 22:53:10 +0530

    referred gfg

---
+++ Linked List/Add one to LL.cpp
@@ -0,0 +1,54 @@
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+Node *reverseList(Node *head) 
+{ 
+    Node * prev = NULL; 
+    Node * current = head; 
+    Node * next; 
+    while (current != NULL) 
+    { 
+        next = current->next; 
+        current->next = prev; 
+        prev = current; 
+        current = next; 
+    } 
+    return prev; 
+} 
+Node* addOneFunc(Node* head){
+    Node* res = head; 
+    Node *temp, *prev = NULL; 
+    int carry = 1, sum; 
+ 
+    while (head != NULL)
+    { 
+        sum = carry + head->data; 
+        carry = (sum >= 10)? 1 : 0; 
+        sum = sum % 10; 
+        head->data = sum;
+        temp = head; 
+        head = head->next; 
+    } 
+    if (carry > 0) 
+        temp->next = new Node(carry); 
+
+    return res; 
+}
+Node* addOne(Node *head) 
+{
+    // Your Code here
+    // return head of list after adding one
+    head = reverseList(head);
+    head = addOneFunc(head);
+    head = reverseList(head);
+    return head;
+}
