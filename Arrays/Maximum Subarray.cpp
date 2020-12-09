5d14ff97677830c8976f4f29c8cfd146e75719a3
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-12-09 15:22:45 +0530

    imp

--- Arrays/Maximum Subarray.cpp
+++ Arrays/Maximum Subarray.cpp
@@ -8,31 +8,15 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 
 #include<iostream>
 #include<algorithm>
-#include<vector>
-using namespace std;
-int kadane(vector<int> &num){
-    int curr_sum = num[0];
-    int max_sum = curr_sum;
-    for(int i = 1; i<num.size(); i++){
-        curr_sum = max(curr_sum + num[i], num[i]);
-        max_sum = max(max_sum, curr_sum);
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int max_so_far = INT_MIN;
+    int curr_sum = 0;
+    for(int i = 0; i<n; i++){
+        curr_sum += arr[i];
+        max_so_far = max(max_so_far, curr_sum);
+        curr_sum = max(curr_sum, 0);
     }
-    return max_sum;
-}
-int main()
- {
-	//code
-	int T;
-	cin>>T;
-	while(T--){
-	    int n;
-	    cin>>n;
-	    vector<int> num(n, 0);
-	    for(int i = 0; i<num.size(); i++){
-	        cin>>num[i];
-	    }
-        cout<<kadane(num)<<endl;
-	}
-	
-	return 0;
+    return max_so_far;
 }
