097e7751975ee7fa921b3e9d318430b049afef0d
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-12-10 13:55:11 +0530

    simple imp

---
+++ Arrays/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn=INT_MAX;
+        int mx=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            mn=min(prices[i],mn);
+            mx=max(prices[i]-mn,mx);
+        }
+       return mx;
+    }
+};
