9d51521bb248f61a8a63a69c195361dd58b2add6
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-12-19 22:55:14 +0530

    imp

--- Strings/Longest Palindrome in a string.cpp
+++ Strings/Longest Palindrome in a string.cpp
@@ -60,3 +60,25 @@ int main() {
     }
     return 0;
 }
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+    int n = s.size(),start=0,maxlen=0;
+    int left=0,right=0;
+    if(n<2) return s;
+    for(int i=0;i<n-1;){
+        left=i, right=i;
+        while(right<n && s[right]==s[right+1]){
+            ++right;
+        } 
+        i=right+1;
+        while(left>=0 && right<n && s[left]==s[right]){
+            --left,++right;
+        } 
+        if(maxlen < right-left-1)   
+            maxlen = right-left-1, start = left+1;         
+        }
+    return s.substr(start,maxlen);
+    }   
+};
