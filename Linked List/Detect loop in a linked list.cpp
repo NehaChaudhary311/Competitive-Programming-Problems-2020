943f3a73300881fefc99c32d2d623581ed9cfa73
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2020-12-27 00:03:14 +0530

    imp

--- Linked List/Detect loop in a linked list.cpp
+++ Linked List/Detect loop in a linked list.cpp
@@ -1,12 +1,22 @@
 int detectloop(Node *head) {
 
     // your code here
-    Node* fptr = head->next;
-    Node* sptr = head;
-    while(sptr != fptr){
-        if(fptr == NULL || fptr->next == NULL) return 0;
-        fptr = fptr->next->next;
-        sptr = sptr->next;
+    if(head == NULL) return 0;
+    Node* slow = head;
+    Node* fast = head;
+    while(true) {
+
+        slow = slow->next;          
+
+        if(fast->next != NULL)
+            fast = fast->next->next; 
+        else
+            return 0;          // next node null => no loop
+
+        if(slow == NULL || fast == NULL) // if either hits null..no loop
+            return 0;
+
+        if(slow == fast) // if the two ever meet...we must have a loop
+            return 1;
     }
-    return 1;
 }
