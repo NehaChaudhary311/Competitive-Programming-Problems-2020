878a091da76ec99ebbc8c797876e3e890bce4713
Neha Chaudhary <44233033+NehaChaudhary311@users.noreply.github.com>
2021-01-27 18:04:37 +0530

    imp gut

--- Trees/Symmetric tree.cpp
+++ Trees/Symmetric tree.cpp
@@ -121,3 +121,53 @@ bool isSymmetric(struct Node* root)
 	if(root == NULL) return true;
 	return isMirror(root->left, root->right);
 }
+
+
+//2ND TRY
+bool isMirror(TreeNode* p, TreeNode* q){
+        if (!p && !q) {
+            return true;
+        }
+        else if (!p || !q) {
+            return false;
+        }
+        
+        if (p->val != q->val) {
+            return false;
+        }
+        
+        return isMirror(p->left,q->right) && isMirror(p->right, q->left); 
+            
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return isMirror(root->left, root->right);
+    }
+
+//APPROACH - ITERATIVE WAY
+bool isSymmetric(TreeNode *root) {
+        TreeNode *left, *right;
+        if (!root)
+            return true;
+        
+        queue<TreeNode*> q1, q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while (!q1.empty() && !q2.empty()){
+            left = q1.front();
+            q1.pop();
+            right = q2.front();
+            q2.pop();
+            if (NULL == left && NULL == right)
+                continue;
+            if (NULL == left || NULL == right)
+                return false;
+            if (left->val != right->val)
+                return false;
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+        }
+        return true;
+    }
